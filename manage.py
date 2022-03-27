7e0856122e5173317014b8ff4769692787259f7e
Shahriyar <contact@shahriyar.dev>
2022-03-27 12:44:48 +0600

    Added manage.py with initdb command and README.md

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 import asyncio
+import click
 
 from basicauth import app
 from basicauth.helpers.db import _get_database_connection, indexes, schemas
 
 
-@app.cli.command()
+@click.group()
+def cli():
+    """The CLI"""
+
+@cli.command()
 def initdb():
     loop = asyncio.get_event_loop()
     con = loop.run_until_complete(_get_database_connection(app))
@@ -13,3 +18,6 @@ def initdb():
         loop.run_until_complete(con.execute(schema))
 
     print("Database initialized")
+
+if __name__ == "__main__":
+    cli()
